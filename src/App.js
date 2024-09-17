dfd33d292615a9d1276851dce21b611aafac8810
HannaFarha <johnnyfarha1@gmail.com>
2024-09-17 11:23:15 +0200

    useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
-
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const[todos, setTodos]= useState(["test"])
   return (
     <div className="App">
       <h2>To Do List</h2>
       <ul>
+        {todos.map((item)=>{return <li>{item}</li>})}
 
 
       </ul>
