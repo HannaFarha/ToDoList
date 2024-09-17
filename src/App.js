a494f2795f2e991652b74da93488eb0caa068f0a
HannaFarha <johnnyfarha1@gmail.com>
2024-09-17 13:11:39 +0200

    new Item

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import './App.css';
 
 function App() {
   const[todos, setTodos]= useState(["test"])
+  const inputRef = useRef()
+  const handleAddTodo=()=>{
+    const text =inputRef.current.value;
+const newItem={completed:false,text}
+
+    setTodos([...todos, newItem])
+    console.log(text)
+  }
   return (
     <div className="App">
       <h2>To Do List</h2>
+      <div className="to-do-container">
       <ul>
-        {todos.map((item)=>{return <li>{item}</li>})}
+        {todos.map((item)=>{return <li>{item.text}</li>})}
 
 
       </ul>
-      <input />
-      <button>Add</button>
+      <input ref={inputRef} placeholder="Enter item..."/>
+      <button onClick={handleAddTodo}>Add</button>
+      </div>
     </div>
   );
 }
