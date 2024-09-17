96e9a7d17839c2ca6e7fc5a7efb5e0694f872106
HannaFarha <johnnyfarha1@gmail.com>
2024-09-17 15:45:10 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,27 +1,52 @@
-import { useRef, useState } from 'react';
-import './App.css';
+import { useRef, useState } from "react";
+import "./App.css";
 
 function App() {
-  const[todos, setTodos]= useState(["test"])
-  const inputRef = useRef()
-  const handleAddTodo=()=>{
-    const text =inputRef.current.value;
-const newItem={completed:false,text}
+  const [todos, setTodos] = useState([]);
 
-    setTodos([...todos, newItem])
-    console.log(text)
+  const inputRef = useRef();
+
+  const handleAddTodo = () => {
+    const text = inputRef.current.value;
+    const newItem = { completed: false, text };
+    setTodos([...todos, newItem]);
+    inputRef.current.value = "";
+  };
+
+  const handleItemDone = (index) => {
+    const newTodos = [...todos];
+    newTodos[index].completed = !newTodos[index].completed;
+    setTodos(newTodos);
+  };
+
+  const handleDeleteItem = (index) => {
+    const newTodos = [...todos];
+    newTodos.splice(index, 1)
+    setTodos(newTodos)
   }
+
   return (
     <div className="App">
       <h2>To Do List</h2>
       <div className="to-do-container">
-      <ul>
-        {todos.map((item)=>{return <li>{item.text}</li>})}
-
-
-      </ul>
-      <input ref={inputRef} placeholder="Enter item..."/>
-      <button onClick={handleAddTodo}>Add</button>
+        <ul>
+          {todos.map(({ text, completed }, index) => {
+            return (
+              <div className="item">
+                <li
+                  className={completed ? "done" : ""}
+                  key={index}
+                  onClick={() => handleItemDone(index)}
+                >
+                  {text}
+                </li>
+                <span onClick={() => handleDeleteItem(index)} className="trash">❌</span>
+              </div>
+            );
+          })}
+        </ul>
+        <input ref={inputRef} placeholder="Enter item..." />
+        <button onClick={handleAddTodo}>Add</button>
       </div>
     </div>
   );
